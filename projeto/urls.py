ba429c03b239fc944bd5f6cad0569074ab7d8e2f
DaniebPrata <danielbprata@gmail.com>
2024-03-10 16:08:10 -0300

    urls.py modification for testing

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def my_view():
+    ...
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('aboute/', my_view),
 ]
