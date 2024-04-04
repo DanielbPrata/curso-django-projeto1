8af8357a01d2f5481aab14e781ffc298c57cf530
DaniebPrata <danielbprata@gmail.com>
2024-04-04 14:20:48 -0300

    recipes home page

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from recipes.views import home, contact, about
 
 # exemplo de apresentação dominio.com/recipes/home, contact, about....
-urlpatterns = [path('', home),  # /home/
-               path('contact/', contact),  # /contact/
-               path('about/', about)  # /about/
-               ]
+urlpatterns = [
+    path('', home),  # /home/
+    path('contact/', contact),  # /contact/
+    path('about/', about)  # /about/
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello, world2.")
+    return render(request, 'Home.html')
 
 
 def contact(request):
