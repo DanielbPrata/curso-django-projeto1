e07ab591d584053314d1ae8e582c0248ea00bae8
DaniebPrata <danielbprata@gmail.com>
2024-04-07 18:47:42 -0300

    follow up

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,23 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Jose',
+        'age': 25,
+        'is_new': True,
+        'fruits': ['Banana', 'Apple', 'Pear'],
+        'numbers': [1, 2, 3, 4, 5],
+        'person': {
+            'name': 'Jose',
+            'age': 25,
+
+        }
+
+    })
 
 
 def contact(request):
-    return HttpResponse("contact page2")
+    return render(request, 'recipes/contact.html')
 
 
 def about(request):
