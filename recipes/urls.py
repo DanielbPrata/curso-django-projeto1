a91bac62b3f7cdb7f34f4be09719b4bf8e0471a8
DaniebPrata <danielbprata@gmail.com>
2024-04-07 18:25:28 -0300

    follow up

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,6 @@ from recipes.views import home, contact, about
 # exemplo de apresentação dominio.com/recipes/home, contact, about....
 urlpatterns = [
     path('', home),
-    path('home/', home),  # /home/
     path('contact/', contact),  # /contact/
     path('about/', about)  # /about/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/home.html')
 
 
 def contact(request):
